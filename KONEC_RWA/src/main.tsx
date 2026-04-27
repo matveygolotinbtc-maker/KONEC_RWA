29b0d3050ef72b0d3cb9d47ee0303ea5439fcc87
matveygolotinbtc-maker <matveygolotinbtc@gmail.com>
2026-04-27 14:26:09 +0300

    Add simple build marker for deployment verification

--- KONEC_RWA/src/main.tsx
+++ KONEC_RWA/src/main.tsx
@@ -8,8 +8,17 @@ const params = new URLSearchParams(window.location.search);
 const mode = params.get('mode');
 const App = mode === 'admin' ? AppAdminMintLive : AppInvestorLive;
 
+function Root() {
+  return (
+    <>
+      <App />
+      <div className="build-marker">build: buy-order-v2</div>
+    </>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
