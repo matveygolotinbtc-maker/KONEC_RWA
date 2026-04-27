6fefbec90987d915d386b68a19a75518dbe14b83
matveygolotinbtc-maker <matveygolotinbtc@gmail.com>
2026-04-27 13:36:52 +0300

    Allow switching between admin and investor screens

--- KONEC_RWA/src/main.tsx
+++ KONEC_RWA/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppInvestorLive from './AppInvestorLive';
+import AppPaymentLive from './AppPaymentLive';
 import './styles.css';
+
+const params = new URLSearchParams(window.location.search);
+const mode = params.get('mode');
+const App = mode === 'admin' ? AppPaymentLive : AppInvestorLive;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppInvestorLive />
+    <App />
   </React.StrictMode>
 );
