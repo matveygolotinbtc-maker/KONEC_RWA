79b1f8f4135e8e62bdbcb7f4ae58952d83caaa04
matveygolotinbtc-maker <matveygolotinbtc@gmail.com>
2026-04-27 15:06:27 +0300

    Switch investor mode to V6 account order

--- KONEC_RWA/src/main.tsx
+++ KONEC_RWA/src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppInvestorLiveV5 from './AppInvestorLiveV5';
+import AppInvestorLiveV6 from './AppInvestorLiveV6';
 import AppAdminMintLive from './AppAdminMintLive';
 import './styles.css';
 
 const params = new URLSearchParams(window.location.search);
 const mode = params.get('mode');
-const App = mode === 'admin' ? AppAdminMintLive : AppInvestorLiveV5;
+const App = mode === 'admin' ? AppAdminMintLive : AppInvestorLiveV6;
 
 function Root() {
   return (
     <>
       <App />
-      <div className="build-marker">build: buy-order-v5</div>
+      <div className="build-marker">build: buy-order-v6</div>
     </>
   );
 }
